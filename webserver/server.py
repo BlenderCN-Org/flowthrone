2602e131582daec5507171f44aee70302c8fc872
vasiliy <karasev00@gmail.com>
2018-05-20 13:53:25 -0700

    Webserver work
    * Add ability to delete results.
    * Add a stupid gross annoying hack to allow video processing.
    * Python style formatting.

--- webserver/server.py
+++ webserver/server.py
@@ -16,57 +16,66 @@ app.config['MAX_CONTENT_LENGTH'] = config.MAX_CONTENT_LENGTH
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 Markdown(app)
 
-""" Front page """
+
 @app.route('/')
 @app.route('/index.html')
 def index():
+    """ Front page """
     return render_template('index.html')
 
-""" Demo page """
+
 @app.route('/demo')
 def demo():
-    return render_template('demo.html', 
-        default_options_pbtxt=config.DEFAULT_OPTICAL_FLOW_OPTIONS)
+    """ Demo page """
+    return render_template(
+        'demo.html', default_options_pbtxt=config.DEFAULT_OPTICAL_FLOW_OPTIONS)
 
-""" Directory listing of runs that have been done. """
+
 @app.route('/examples')
 def examples():
+    """ Directory listing of runs that have been done. """
     uuids = [d for d in os.listdir(config.EXAMPLES_FOLDER) \
             if utils.result_is_valid(config.EXAMPLES_FOLDER, d)]
     uuids.sort()
     uuids.reverse()
     return render_template('examples.html', results=uuids)
 
-""" Documentation page """
+
 @app.route('/docs')
 def docs():
+    """ Documentation page """
     return render_template('docs.html')
 
-""" Shorthand for reporting catasrophes. """
-def internal_error():
-    return render_template('response.html', content='Internal error', error=True)
 
-""" View for content that's been uploaded. """
+def internal_error():
+    """ Shorthand for reporting catasrophes. """
+    return render_template(
+        'response.html', content='Internal error', error=True)
+
+
 @app.route('/uploads/<path:filename>')
 def uploaded_file(filename):
+    """ View for content that's been uploaded. """
     return send_from_directory(config.UPLOAD_FOLDER, filename)
+
 
 @app.route('/submit_task', methods=['POST'])
 def submit_task():
     if not request.method == 'POST':
         return index()
-    
+
     # Check that the user provided two valid images or a video.
     valid, error_msg = utils.request_is_valid(request)
     if not valid:
-        return render_template('response.html', content=error_msg, error=True) 
-    
+        return render_template('response.html', content=error_msg, error=True)
+
     # Task is valid, so schedule it and return the identifier.
     task_uuid = utils.schedule_task(request, config.EXAMPLES_FOLDER)
     if not task_uuid:
         return internal_error()
 
-    success_msg = ("Successfully uploaded files and scheduled a task '{}'.").format(task_uuid)
+    success_msg = ("Successfully uploaded files and scheduled a task '{}'."
+                   ).format(task_uuid)
     return render_template('response.html', content=success_msg, error=False)
 
 
@@ -77,42 +86,54 @@ def results():
     results.sort()
     return render_template('results.html', results=results)
 
+
 @app.route('/result/<result_id>')
 def result(result_id):
     p = os.path.join(config.RESULTS_FOLDER, result_id)
-    result_pbtxt = [os.path.join(p, f) for f in os.listdir(p) if f.endswith('pbtxt')][0]
+    result_pbtxt = [
+        os.path.join(p, f) for f in os.listdir(p) if f.endswith('pbtxt')
+    ][0]
     stats = utils.read_evaluation_results_pbtxt(result_pbtxt)
 
-    return render_template('result.html', stats=stats, result_id=result_id) 
+    return render_template('result.html', stats=stats, result_id=result_id)
 
-""" Returns a JSON with results for a given id. """
+
+@app.route('/delete_example/<result_id>')
+def delete_example(result_id, methods=['GET']):
+    utils.delete_result(config.EXAMPLES_FOLDER, result_id)
+    return index()
+
+
 @app.route('/load_example/<result_id>')
 def load_example(result_id):
+    """ Returns a JSON with results for a given id. """
     result_data = {
         'id': result_id,
         'stderr': '',
         'result_image_filename': '',
         'result_video_filename': '',
     }
-    
+
     result_image_filename = utils.result_image_filename_or_none(
-            config.EXAMPLES_FOLDER, result_id)
+        config.EXAMPLES_FOLDER, result_id)
     result_video_filename = utils.result_video_filename_or_none(
-            config.EXAMPLES_FOLDER, result_id)
+        config.EXAMPLES_FOLDER, result_id)
     result_stderr_filename = utils.result_stderr_filename_or_none(
-            config.EXAMPLES_FOLDER, result_id)
+        config.EXAMPLES_FOLDER, result_id)
 
     if result_stderr_filename is not None:
-        full_filename = os.path.join(
-            config.EXAMPLES_FOLDER, result_stderr_filename)
+        full_filename = os.path.join(config.EXAMPLES_FOLDER,
+                                     result_stderr_filename)
         result_data['stderr'] = open(full_filename).read()
-    if result_image_filename is not None: 
-        result_data['result_image_filename'] = os.path.join('examples', result_image_filename)
-    if result_video_filename is not None: 
-        result_data['result_video_filename'] = os.path.join('examples', result_video_filename)
+    if result_image_filename is not None:
+        result_data['result_image_filename'] = os.path.join(
+            'examples', result_image_filename)
+    if result_video_filename is not None:
+        result_data['result_video_filename'] = os.path.join(
+            'examples', result_video_filename)
 
     return json.dumps(result_data)
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-
--- webserver/utils.py
+++ webserver/utils.py
@@ -5,71 +5,83 @@ import base64
 import time
 import subprocess
 import uuid
+import shutil
+
 
 def file_is_allowed(filename, allowed_extensions):
     return any([filename.lower().endswith(ext.lower()) \
         for ext in allowed_extensions])
 
-""" Returns a pair (True/False, error message). """
+
 def request_has_valid_images(files):
+    """ Returns a pair (True/False, error message). """
     # check if images are specified.
     for k in ['image1', 'image2']:
         if k not in files or files[k].filename == '' or not files[k]:
             return False, 'One or more images were not specified.'
-    # if both images are specified, ensure that they have valid extensions 
+    # if both images are specified, ensure that they have valid extensions
     for k in ['image1', 'image2']:
-        if not file_is_allowed(files[k].filename, config.ALLOWED_IMAGE_EXTENSIONS):
+        if not file_is_allowed(files[k].filename,
+                               config.ALLOWED_IMAGE_EXTENSIONS):
             return False, \
                 "Image was specified: '{}', but is not an allowed file.".format(
                     files[k].filename)
     return True, ''
 
-""" Returns a pair (True/False, error message). """
+
 def request_has_valid_video(files):
+    """ Returns a pair (True/False, error message). """
     # check if video is specified.
     if 'video' not in files or files['video'].filename == '' or not files['video']:
         return False, 'Video was not specified.'
-    if not file_is_allowed(files['video'].filename, config.ALLOWED_VIDEO_EXTENSIONS):
+    if not file_is_allowed(files['video'].filename,
+                           config.ALLOWED_VIDEO_EXTENSIONS):
         return False, \
             "Video was specified: '{}', but is not an allowed file.".format(
                 files['video'].filename)
     return True, ''
 
-""" Returns a pair (True/False, error message). """
+
 def request_is_valid(request):
+    """ Returns a pair (True/False, error message). """
     has_images, err_images = request_has_valid_images(request.files)
     has_video, err_video = request_has_valid_video(request.files)
-    
+
     if has_images and has_video:
         return False, ("Specifying both a pair of images and a video is "
                        "ambiguous -- should specify only one.")
     if not has_images and not has_video:
         return False, '\n'.join([err_images, err_video])
-    return True, '' 
+    return True, ''
+
 
 def upload_file(file, upload_folder):
     filename = os.path.join(upload_folder, secure_filename(file.filename))
     file.save(filename)
     return filename
 
-""" Uploads and returns a list of uploaded files. If images were sent, the
-    list will have two elements; otherwise it will be a singleton. 
+
+def upload_files(request, upload_folder):
+    """ Uploads and returns a list of uploaded files. If images were sent, the
+    list will have two elements; otherwise it will be a singleton.
     The user must check that the request is valid prior to running this.
     """
-def upload_files(request, upload_folder):
     uploaded_files = []
     has_images, _ = request_has_valid_images(request.files)
     if has_images:
-        return [upload_file(request.files['image1'], upload_folder),
-                upload_file(request.files['image2'], upload_folder)]
-    
+        return [
+            upload_file(request.files['image1'], upload_folder),
+            upload_file(request.files['image2'], upload_folder)
+        ]
+
     has_video, _ = request_has_valid_video(request.files)
     if has_video:
         return [upload_file(request.files['video'], upload_folder)]
 
+
 def upload_options_pbtxt(request, output_filename):
     # TODO: check that pbtxt is valid.
-    with open(output_filename,'w') as fid:
+    with open(output_filename, 'w') as fid:
         fid.write(request.form['flowthroneoptions'])
 
 
@@ -77,39 +89,42 @@ def optical_flow_binary():
     return os.path.join(config.FLOWTHRONE_BUILD_PATH, './optical_flow_tool')
 
 
-def get_optical_flow_tool_images_command(img1_filename, img2_filename, 
-        options_pbtxt_filename, output_filename, output_log_filename):
-    return [optical_flow_binary(), 
-        '--img1', img1_filename, '--img2', img2_filename,
-        '--options', options_pbtxt_filename, 
-        '--output', output_filename,
-        '--alsologtostderr=1', '&>', output_log_filename]
-
-
-def get_optical_flow_tool_video_command(video_filename, 
-        options_pbtxt_filename, output_filename, output_log_filename):
+def get_optical_flow_tool_images_command(img1_filename, img2_filename,
+                                         options_pbtxt_filename,
+                                         output_filename, output_log_filename):
     return [
-        optical_flow_binary(), 
-        '--video', video_filename,
-        '--options', options_pbtxt_filename, 
-        '--output', output_filename,
-        '--alsologtostderr=1', '&>', output_log_filename]
+        optical_flow_binary(), '--img1', img1_filename, '--img2',
+        img2_filename, '--options', options_pbtxt_filename, '--output',
+        output_filename, '--alsologtostderr=1', '&>', output_log_filename
+    ]
 
 
-def get_optical_flow_tool_command(input_files, options_pbtxt_filename, output_filename, output_log_filename):
+def get_optical_flow_tool_video_command(video_filename, options_pbtxt_filename,
+                                        output_filename, output_log_filename):
+    return [
+        optical_flow_binary(), '--video', video_filename, '--options',
+        options_pbtxt_filename, '--output', output_filename,
+        '--alsologtostderr=1', '&>', output_log_filename
+    ]
+
+
+def get_optical_flow_tool_command(input_files, options_pbtxt_filename,
+                                  output_filename, output_log_filename):
     if len(input_files) == 2:
         # Must be a pair of images then!
         return get_optical_flow_tool_images_command(
-            input_files[0], input_files[1], options_pbtxt_filename, output_filename, output_log_filename)
+            input_files[0], input_files[1], options_pbtxt_filename,
+            output_filename, output_log_filename)
     if len(input_files) == 1:
         return get_optical_flow_tool_video_command(
-            input_files[0], options_pbtxt_filename, output_filename, output_log_filename)
-    return None 
+            input_files[0], options_pbtxt_filename, output_filename,
+            output_log_filename)
+    return None
 
 
-""" Checks for files in `upload_folder/result_id/match` and returns
-    'result_id/match' (if there is a unique match). """
 def result_filename_or_none(upload_folder, result_id, match):
+    """ Checks for files in `upload_folder/result_id/match` and returns
+        'result_id/match' (if there is a unique match). """
     if not result_id in os.listdir(upload_folder):
         return None
     files = os.listdir(os.path.join(upload_folder, result_id))
@@ -118,6 +133,7 @@ def result_filename_or_none(upload_folder, result_id, match):
         return filenames[0]
     else:
         return None
+
 
 def result_image_filename_or_none(upload_folder, result_id):
     return result_filename_or_none(\
@@ -137,44 +153,81 @@ def result_stderr_filename_or_none(upload_folder, result_id):
 def result_is_valid(upload_folder, result_id):
     if not result_id in os.listdir(upload_folder):
         return False
-    
+
     files = os.listdir(os.path.join(upload_folder, result_id))
     log_filenames = [f for f in files if f == config.OUTPUT_LOG_FILENAME]
     # It is permissible not to have an output image/video. As long as the log
     # has been generated, let's say that the result is valid.
     return len(log_filenames) == 1
 
+
+def delete_result(upload_folder, result_id):
+    if not result_id in os.listdir(upload_folder):
+        print "Could not delete {} (doesn't exist? in {})".format(
+            result_id, upload_folder)
+    p = os.path.join(upload_folder, result_id)
+    shutil.rmtree(p, ignore_errors=True)
+
+
 def base64_encoded_image(image_bytes, ext):
     return 'data:image/' + ext + ';base64,' + base64.b64encode(image_bytes)
+
 
 def task_identifier():
     t = time.time()
     return '{:10d}-{}'.format(long(t), uuid.uuid1(clock_seq=long(t)).hex[0:8])
 
-""" Schedules a task and returns an id on success, and 'None' on failure. """
+
+def convert_avi_to_webm(input_filename, output_filename):
+    cmd = [
+        'ffmpeg',
+        '-i',
+        input_filename,
+        '-c:v',
+        'libvpx',
+        '-qmin',
+        str(0),
+        '-qmax',
+        str(1),
+        '-crf',
+        str(1),
+        '-q:v',
+        '1',
+        output_filename,
+    ]
+    print cmd
+    subprocess.call(cmd)
+
+
 def schedule_task(request, upload_folder):
-    print upload_folder
+    """ Schedules a task and returns an id on success, and 'None' on failure. """
     task_uuid = task_identifier()
     task_folder = os.path.join(upload_folder, task_uuid)
-    print task_folder
     if not os.path.exists(task_folder):
         try:
             os.mkdir(task_folder)
         except:
             print "Could not create {}".format(task_folder)
             return None
-    
+
     options_pbtxt = os.path.join(task_folder, 'options.pbtxt')
     upload_options_pbtxt(request, options_pbtxt)
-    
+
     uploaded_files = upload_files(request, task_folder)
     if len(uploaded_files) == 2:
-        output_filename = os.path.join(task_folder, config.OUTPUT_IMAGE_FILENAME)
+        output_filename = os.path.join(task_folder,
+                                       config.OUTPUT_IMAGE_FILENAME)
     else:
-        output_filename = os.path.join(task_folder, config.OUTPUT_VIDEO_FILENAME)
+        output_filename = os.path.join(task_folder,
+                                       config.OUTPUT_VIDEO_FILENAME)
+        # Opencv is unbelievably frustrating. Even with ffmpeg built from
+        # source with libvpx support, opening a video for writing fails.
+        # Instead, first write an 'avi' file, and then convert it to webm
+        # below.
+        output_filename = output_filename.replace('webm', 'avi')
     output_log_filename = os.path.join(task_folder, config.OUTPUT_LOG_FILENAME)
-    cmd = get_optical_flow_tool_command(uploaded_files, options_pbtxt, output_filename, output_log_filename)
-    print cmd
+    cmd = get_optical_flow_tool_command(uploaded_files, options_pbtxt,
+                                        output_filename, output_log_filename)
     # This is really bad, since it will:
     #   (a) block the website until everything is done.
     #   (b) multiple users accessing the website simultaneously will crash the
@@ -182,12 +235,18 @@ def schedule_task(request, upload_folder):
     # It would have been much better to either:
     #   (a) launch the task with Celery
     #   (b) launch it async with popen
+    print cmd
     with open(output_log_filename, 'w') as f:
         subprocess.call(cmd, stdout=f, stderr=f)
-    return task_uuid 
 
-""" Parses pbtxt file and returns EvaluationOutput message. """
+    if output_filename.endswith('avi'):
+        convert_avi_to_webm(output_filename,
+                            output_filename.replace('avi', 'webm'))
+    return task_uuid
+
+
 def read_evaluation_results_pbtxt(filename):
+    """ Parses pbtxt file and returns EvaluationOutput message. """
     import flowthrone_pb2
     import google.protobuf.text_format
     result_str = open(filename, 'r').read()
